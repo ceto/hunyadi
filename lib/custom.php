689928fd93bda94f8d487fd24b80df0bf0eb2d64
Gabor Szabo <szabogabi@gmail.com>
2015-10-19 13:22:31 +0200

    termeklap

--- lib/custom.php
+++ lib/custom.php
@@ -70,98 +70,177 @@ function hu_metaboxes() {
      * Initiate the metabox
      */
     $cmb_page = new_cmb2_box( array(
-        'id'            => 'content_metabox',
-        'title'         => __( 'Függőleges tartalmi sávok', 'cmb2' ),
-        'object_types'  => array( 'page','referencia' ), // Post type
-        'context'       => 'normal',
-        'priority'      => 'high',
-        'show_names'    => true, // Show field names on the left
-        // 'cmb_styles' => false, // false to disable the CMB stylesheet
-        // 'closed'     => true, // Keep the metabox closed by default
+      'id'            => 'content_metabox',
+      'title'         => __( 'Függőleges tartalmi sávok', 'cmb2' ),
+      'object_types'  => array( 'page','referencia' ), // Post type
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+      // 'cmb_styles' => false, // false to disable the CMB stylesheet
+      'closed'     => true, // Keep the metabox closed by default
     ) );
 
 
     $group_field_id = $cmb_page->add_field( array(
-        'id'          => 'page_repeat_group',
-        'type'        => 'group',
-        'description' => __( 'Szerkeszthető oldalsávok', 'cmb2' ),
-        'options'     => array(
-            'group_title'   => __( 'Oldalsáv {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
-            'add_button'    => __( 'Új oldalsáv hozzáadása', 'cmb2' ),
-            'remove_button' => __( 'Oldalsáv törlése', 'cmb2' ),
-            'sortable'      => true, // beta
-        ),
+      'id'          => 'page_repeat_group',
+      'type'        => 'group',
+      'description' => __( 'Szerkeszthető oldalsávok', 'cmb2' ),
+      'options'     => array(
+          'group_title'   => __( 'Oldalsáv {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+          'add_button'    => __( 'Új oldalsáv hozzáadása', 'cmb2' ),
+          'remove_button' => __( 'Oldalsáv törlése', 'cmb2' ),
+          'sortable'      => true, // beta
+      ),
     ) );
 
     $cmb_page->add_group_field( $group_field_id, array(
-        'name' => 'Szélesség',
-        'id'   => 'width',
-        'type' => 'radio_inline',
-        'options'          => array(
-          'normal' => __( 'Normál', 'cmb' ),
-          'wide' => __( 'Széles', 'cmb' ),
-        ),
+      'name' => 'Szélesség',
+      'id'   => 'width',
+      'type' => 'radio_inline',
+      'options'          => array(
+        'normal' => __( 'Normál', 'cmb' ),
+        'wide' => __( 'Széles', 'cmb' ),
+      ),
     ) );
 
     $cmb_page->add_group_field( $group_field_id, array(
-        'name' => 'Megjelenés',
-        'id'   => 'colwidth',
-        'type' => 'radio_inline',
-        'options'          => array(
-          'full' => __( 'Egy oszlopos', 'cmb' ),
-          'half' => __( 'Két oszlopos', 'cmb' ),
-        ),
+      'name' => 'Megjelenés',
+      'id'   => 'colwidth',
+      'type' => 'radio_inline',
+      'options'          => array(
+        'full' => __( 'Egy oszlopos', 'cmb' ),
+        'half' => __( 'Két oszlopos', 'cmb' ),
+      ),
     ) );
 
 
     $cmb_page->add_group_field( $group_field_id, array(
-        'name' => 'Tartalom',
-        'description' => 'Szöveges tartalom',
-        'id'   => 'content',
-        'type' => 'wysiwyg',
+      'name' => 'Tartalom',
+      'description' => 'Szöveges tartalom',
+      'id'   => 'content',
+      'type' => 'wysiwyg',
     ) );
 
     $cmb_page->add_group_field( $group_field_id, array(
-        'name' => 'Tartalom 2',
-        'description' => '2. oszlop szöveges tartalma',
-        'id'   => 'content2',
-        'type' => 'wysiwyg',
+      'name' => 'Tartalom 2',
+      'description' => '2. oszlop szöveges tartalma',
+      'id'   => 'content2',
+      'type' => 'wysiwyg',
     ) );
 
 
     $cmb_related = new_cmb2_box( array(
-        'id'            => 'related_metabox',
-        'title'         => __( 'Kapcsolódó írások, termékek, referencia', 'cmb2' ),
-        'object_types'  => array( 'page', ), // Post type
-        'context'       => 'normal',
-        'priority'      => 'high',
-        'show_names'    => true, // Show field names on the left
-        // 'cmb_styles' => false, // false to disable the CMB stylesheet
-        // 'closed'     => true, // Keep the metabox closed by default
+      'id'            => 'related_metabox',
+      'title'         => __( 'Kapcsolódó írások, termékek, referencia', 'cmb2' ),
+      'object_types'  => array( 'page', ), // Post type
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+      // 'cmb_styles' => false, // false to disable the CMB stylesheet
+      // 'closed'     => true, // Keep the metabox closed by default
     ) );
 
 
     $cmb_related->add_field( array(
-        'id'          => 'related_posts',
-        'name'         => __( 'Kapcsolódó írások', 'cmb2' ),
-        'type'  => 'multicheck_inline',
-        'options' => hu_optionlist('post')
+      'id'          => 'related_posts',
+      'name'         => __( 'Kapcsolódó írások', 'cmb2' ),
+      'type'  => 'multicheck_inline',
+      'options' => hu_optionlist('post')
     ) );
 
     $cmb_related->add_field( array(
-        'id'          => 'related_references',
-        'name'         => __( 'Kapcsolódó referenciák', 'cmb2' ),
-        'type'  => 'multicheck_inline',
-        'options' => hu_optionlist('referencia')
+      'id'          => 'related_references',
+      'name'         => __( 'Kapcsolódó referenciák', 'cmb2' ),
+      'type'  => 'multicheck_inline',
+      'options' => hu_optionlist('referencia')
     ) );
 
     $cmb_related->add_field( array(
-        'id'          => 'related_products',
-        'name'         => __( 'Kapcsolódó termékek', 'cmb2' ),
-        'type'  => 'multicheck_inline',
-        'options' => hu_optionlist('termek')
+      'id'          => 'related_products',
+      'name'         => __( 'Kapcsolódó termékek', 'cmb2' ),
+      'type'  => 'multicheck_inline',
+      'options' => hu_optionlist('termek')
     ) );
 
 
+    /********* Termék oldali mezők ******/
+
+    $cmb_product = new_cmb2_box( array(
+      'id'            => 'product_metabox',
+      'title'         => __( 'Termékvariánsok', 'cmb2' ),
+      'object_types'  => array( 'termek' ), // Post type
+      'context'       => 'normal',
+      'priority'      => 'high',
+      'show_names'    => true, // Show field names on the left
+      // 'cmb_styles' => false, // false to disable the CMB stylesheet
+      // 'closed'     => true, // Keep the metabox closed by default
+    ) );
+
+    $product_group_field_id = $cmb_product->add_field( array(
+      'id'          => 'product_repeat_group',
+      'type'        => 'group',
+      'description' => __( 'Termékvariánsok', 'cmb2' ),
+      'options'     => array(
+          'group_title'   => __( 'Termékvariáns {#}', 'cmb2' ), // since version 1.1.4, {#} gets replaced by row number
+          'add_button'    => __( 'Új termékvariáns hozzáadása', 'cmb2' ),
+          'remove_button' => __( 'Termékvariáns törlése', 'cmb2' ),
+          'sortable'      => true, // beta
+      ),
+    ) );
+
+    $cmb_product->add_group_field( $product_group_field_id, array(
+      'name' => 'Termék neve',
+      'id'   => 'name',
+      'type' => 'text',
+    ) );
+
+    $cmb_product->add_group_field( $product_group_field_id, array(
+      'name' => 'Leírás',
+      'description' => 'A termékvariáns szöveges leírása',
+      'id'   => 'description',
+      'type' => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => '7',
+      )
+    ) );
+
+    $cmb_product->add_group_field( $product_group_field_id, array(
+      'name' => 'Termékfotó',
+      'id'   => 'prodphoto',
+      'type' => 'file',
+    ) );
+
+    $cmb_product->add_group_field( $product_group_field_id, array(
+      'name' => 'Termék paraméterek',
+      'description' => 'Bekezdésenként egy paraméter pár az alábbi formában: <strong>Paraméter neve|Paraméter értéke</strong>',
+      'id'   => 'params',
+      'type' => 'wysiwyg',
+      'options' => array(
+        'textarea_rows' => '7',
+      )
+    ) );
+
+    $cmb_product->add_group_field( $product_group_field_id, array(
+        'name' => 'Csatolt fájlok letöltésre',
+        'description' => 'Válassz a médiatárból',
+        'id'   => 'dlfiles',
+        'type' => 'file_list',
+        'options' => array(
+        //   'add_upload_files_text' => 'Replacement', // default: "Add or Upload Files"
+        //   'remove_image_text' => 'Replacement', // default: "Remove Image"
+        //   'file_text' => 'Replacement', // default: "File:"
+        //   'file_download_text' => 'Replacement', // default: "Download"
+        //   'remove_text' => 'Replacement', // default: "Remove"
+        ),
+    ) );
+
+}
+
+
+function adatlaposit($content) {
+  $newhtml = str_replace('<p>', '<dt>', wpautop($content));
+  $newhtml= str_replace('</p>', '</dd>', $newhtml);
+  $newhtml= str_replace('|', '</dt><dd>', $newhtml);
+  return '<dl>'.$newhtml.'</dl>';
 
 }
--- single-termek.php
+++ single-termek.php
@@ -1,22 +1,62 @@
 <?php while (have_posts()) : the_post(); ?>
-  <style>
-    #banner {
-      background-image: url('<?= get_stylesheet_directory_uri(); ?>/dist/images/aramtorony.jpg');
-    }
-  </style>
   <section class="banner" role="banner" id="banner">
     <?php get_template_part('templates/page', 'header'); ?>
   </section>
   <main class="main" role="main">
+
     <section class="pagesection">
       <div class="row">
         <div class="columns medium-8 medium-centered">
-          <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis atque autem quas, debitis, libero ducimus vel. Ab itaque porro quibusdam perferendis! Totam voluptates quaerat quisquam porro officia eos sed, aliquam.</p>
+          <?php the_content(); ?>
         </div>
       </div>
     </section>
 
-    <section class="pagesection pagesection--lighten">
+    <?php if ( get_post_meta( get_the_ID(), 'product_repeat_group', true ) ) {
+      $prod_variants = get_post_meta( get_the_ID(), 'product_repeat_group', true );
+      foreach ( (array) $prod_variants as $key => $entry ) { ?>
+        <section id="<?= sanitize_title( $entry['name'] ); ?>" class="pagesection pagesection--prodvariant">
+          <div class="row">
+            <div class="columns medium-10 medium-centered">
+              <div class="row">
+                <div class="columns medium-4 <?= ($key%2!=0)?'medium-push-8':'' ?>">
+                  <?php if ($entry['prodphoto']) : ?>
+                    <?php
+                      $ima = $entry['prodphoto_id'];
+                      $imci_s = wp_get_attachment_image_src( $ima, 'small');
+                      $imci_m = wp_get_attachment_image_src( $ima, 'medium');
+                      $imci = wp_get_attachment_image_src( $ima, 'large');
+                    ?>
+                    <figure class="prodvariant__photo">
+                      <img src="<?= $imci_s['0']; ?>" alt="">
+                    </figure>
+                  <?php endif; ?>
+                </div>
+                <div class="columns medium-8 <?= ($key%2!=0)?'medium-pull-4':'' ?>">
+                  <div class="prodvarian__content">
+                    <h2><?= $entry['name'] ?></h2>
+                    <?= wpautop($entry['description']); ?>
+
+                    <h3><a href="#">Adatlap</a></h3>
+                    <?= adatlaposit($entry['params']); ?>
+                    <h3><a href="#">Letölthető anyagok</a></h3>
+                    <?php $dlfiles = $entry['dlfiles']; ?>
+                    <?php foreach ( $dlfiles as $csat_id => $csat_url ) : ?>
+                        <?php $csat=get_post( $csat_id ) ?>
+                       <a class="dlfile" href="<?= $csat_url; ?>"><?= $csat->post_title; ?></a>
+                    <?php endforeach; ?>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+    <?php } } ?>
+
+<!--
+    <section class="pagesection pagesection--lighten pagesection--lighten">
       <div class="row">
         <div class="columns medium-8 medium-centered">
             <h2>HEKA berendezések adatlapja</h2>
@@ -46,81 +86,10 @@
                   </dl>
                 </div>
               </li>
-              <li class="accordion-navigation">
-                <a href="#panel2a">HEKA-8 automatikus fázisjavító berendezés</a>
-                <div id="panel2a" class="content">
-                  <dl>
-                    <dt>Berendezés típusa</dt>
-                    <dd>HEKA-1 x/440/IC5/- </dd>
-                    <dt>Alkalmazási hely</dt>
-                    <dd>Normál és alacsonyan szennyezett hálózatra (THD(I) < 20%)</dd>
-                    <dt>Névleges teljesítmény (415V-on)</dt>
-                    <dd>x kvar</dd>
-                    <dt>Bővíthető (igen/nem):</dt>
-                    <dd>Igen</dd>
-                    <dt>Névleges szigetelési / hálózati / kondenzátor feszültség</dt>
-                    <dd>500V / 400V / 440V</dd>
-                    <dt>Kondenzátor típusa</dt>
-                    <dd>PCB mentes, 3 fázisú, Δ kapcsolású, Un=440V-os, beépített kisütőellenállással, N2 gáztöltésű (száraz), öngyógyuló dielektrikummal, serlegházas kivitelű, sorkapocs bekötésű</dd>
-                    <dt>Vezérlő autoatika</dt>
-                    <dd>Mikroprocesszor vezérlésű, állandó digitális cosfi, feszültség, áram kijelzéssel</dd>
-                    <dt>Mágneskapcsoló</dt>
-                    <dd>3 fázisú, kondenzátor-mágneskapcsoló, a bekapcsolási áramlökések korlátozására siettetett zárású segédérintkezővel kapcsolt csillapító ellenállással vannak szerelve</dd>
-                  </dl>
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#panel3a">HEKA tipus</a>
-                <div id="panel3a" class="content">
-                  <dl>
-                    <dt>Berendezés típusa</dt>
-                    <dd>HEKA-1 x/440/IC5/- </dd>
-                    <dt>Alkalmazási hely</dt>
-                    <dd>Normál és alacsonyan szennyezett hálózatra (THD(I) < 20%)</dd>
-                    <dt>Névleges teljesítmény (415V-on)</dt>
-                    <dd>x kvar</dd>
-                    <dt>Bővíthető (igen/nem):</dt>
-                    <dd>Igen</dd>
-                    <dt>Névleges szigetelési / hálózati / kondenzátor feszültség</dt>
-                    <dd>500V / 400V / 440V</dd>
-                    <dt>Kondenzátor típusa</dt>
-                    <dd>PCB mentes, 3 fázisú, Δ kapcsolású, Un=440V-os, beépített kisütőellenállással, N2 gáztöltésű (száraz), öngyógyuló dielektrikummal, serlegházas kivitelű, sorkapocs bekötésű</dd>
-                    <dt>Vezérlő autoatika</dt>
-                    <dd>Mikroprocesszor vezérlésű, állandó digitális cosfi, feszültség, áram kijelzéssel</dd>
-                    <dt>Mágneskapcsoló</dt>
-                    <dd>3 fázisú, kondenzátor-mágneskapcsoló, a bekapcsolási áramlökések korlátozására siettetett zárású segédérintkezővel kapcsolt csillapító ellenállással vannak szerelve</dd>
-                  </dl>
-                </div>
-              </li>
-              <li class="accordion-navigation">
-                <a href="#panel4a">HEKA-2 folytott automatikás berendezés </a>
-                <div id="panel4a" class="content">
-                  <dl>
-                    <dt>Berendezés típusa</dt>
-                    <dd>HEKA-1 x/440/IC5/- </dd>
-                    <dt>Alkalmazási hely</dt>
-                    <dd>Normál és alacsonyan szennyezett hálózatra (THD(I) < 20%)</dd>
-                    <dt>Névleges teljesítmény (415V-on)</dt>
-                    <dd>x kvar</dd>
-                    <dt>Bővíthető (igen/nem):</dt>
-                    <dd>Igen</dd>
-                    <dt>Névleges szigetelési / hálózati / kondenzátor feszültség</dt>
-                    <dd>500V / 400V / 440V</dd>
-                    <dt>Kondenzátor típusa</dt>
-                    <dd>PCB mentes, 3 fázisú, Δ kapcsolású, Un=440V-os, beépített kisütőellenállással, N2 gáztöltésű (száraz), öngyógyuló dielektrikummal, serlegházas kivitelű, sorkapocs bekötésű</dd>
-                    <dt>Vezérlő autoatika</dt>
-                    <dd>Mikroprocesszor vezérlésű, állandó digitális cosfi, feszültség, áram kijelzéssel</dd>
-                    <dt>Mágneskapcsoló</dt>
-                    <dd>3 fázisú, kondenzátor-mágneskapcsoló, a bekapcsolási áramlökések korlátozására siettetett zárású segédérintkezővel kapcsolt csillapító ellenállással vannak szerelve</dd>
-                  </dl>
-                </div>
-              </li>
             </ul>
-
-
         </div>
       </div>
-    </section>
+    </section> -->
 
 
 
