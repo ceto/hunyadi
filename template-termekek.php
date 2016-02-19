4e8700b20c602ee7a2bd9aa894242e7882a65a28
Gabor Szabo <szabogabi@gmail.com>
2016-02-19 11:05:17 +0100

    fejek

--- template-termekek.php
+++ template-termekek.php
@@ -10,16 +10,48 @@
     <?php get_template_part('templates/listpage', 'header'); ?>
   </section>
   <main class="main" role="main">
+    <section class="pagesection pagesection--intro">
+      <div class="row">
+        <div class="columns medium-8 columns medium-centered">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </section>
 
-  <?php
+  <div class="ps">
+    <div class="row">
+      <div class="columns medium-10 medium-centered">
+        <ul class="medium-block-grid-2 termekgrid">
+          <?php  $szakteruletek = get_terms( 'field', array());
+           if ( ! empty( $szakteruletek ) && ! is_wp_error( $szakteruletek ) ) : ?>
+            <?php foreach ( $szakteruletek as $field ) :?>
+              <li id="termekblokk--<?= sanitize_title( $field->name ); ?>" class="">
+                    <h2 class="termekblokk__title"><?= $field->name; ?></h2>
+                    <?= term_description( $field->term_id,'field') ?>
+                    <a class="button small" href="<?= get_term_link($field->term_id); ?>#termekblokk">Mutasd a termékeket</a>
+             </li>
+            <?php endforeach; ?>
+          <?php endif; ?>
+        </ul>
 
-    $szakteruletek = get_terms( 'field', array(
-      'parent' => 0
-    ));
-    //print_r($szakteruletek);
+      </div>
+    </div>
+  </div>
 
-   //$szakteruletek = get_terms( 'field' );
+
+
+
+  </main><!-- /.main -->
+<?php endwhile; ?>
+
+
+
+
+
+  <?php /*
+    $szakteruletek = get_terms( 'field', array());
    if ( ! empty( $szakteruletek ) && ! is_wp_error( $szakteruletek ) ) : ?>
+
 
     <?php foreach ( $szakteruletek as $field ) :?>
       <section id="termekblokk--<?= sanitize_title( $field->name ); ?>" class="pagesection  pagesection--termekblokk">
@@ -30,42 +62,8 @@
             <a class="button small" href="<?= get_term_link($field->term_id); ?>#termekblokk">Mutasd a termékeket</a>
           </div>
         </div>
-
-<!--         <div class="row">
-          <div class="columns medium-10 medium-centered">
-            <?php
-              $the_prods = new WP_Query ( array (
-                'post_type' => 'termek',
-                'nopaging' => true,
-                'posts_per_page' => -1,
-                'tax_query' => array(
-                    array(
-                      'taxonomy' => 'field',
-                      'field'    => 'id',
-                      'terms'    => $field->term_id
-                    ),
-                  ),
-              ));
-            ?>
-            <?php if ($the_prods->post_count>0) : ?>
-              <ul class="block-grid-1 small-block-grid-2 medium-block-grid-2 large-block-grid-3 miniprodblock">
-                <?php while ($the_prods->have_posts()) : $the_prods->the_post(); ?>
-                  <?php get_template_part('templates/mini', 'prod' ); ?>
-                <?php endwhile; ?>
-              </ul>
-            <?php endif; ?>
-          </div>
-        </div> -->
-
-
-      </section>
-
-
+     </section>
     <?php endforeach; ?>
 
 
-    <?php endif; ?>
-
-
-  </main><!-- /.main -->
-<?php endwhile; ?>
+    <?php endif; */?>
--- templates/banner-bg.php
+++ templates/banner-bg.php
@@ -1,23 +1,34 @@
-<?php if (has_post_thumbnail()) : ?>
-<?php
-  $ima =  get_post_thumbnail_id();
-  $imci_s = wp_get_attachment_image_src( $ima, 'banner_small');
-  $imci_m = wp_get_attachment_image_src( $ima, 'banner_medium');
-  $imci = wp_get_attachment_image_src( $ima, 'banner');
-?>
-<style>
-  .banner {
-    background-image: url('<?= $imci_s['0']; ?>');
-  }
-  @media only screen and (min-width: 768px) {
+  <?php
+    $ok=false;
+    if (is_archive() || is_search() || is_home()) {
+      $ima =  get_post_thumbnail_id(get_option( 'page_for_posts' ));$ok=true;
+    } elseif (is_page_template('template-termekek.php')) {
+      $ima =  get_post_thumbnail_id(18); $ok=true;
+    } elseif (has_post_thumbnail()) {
+        $ima =  get_post_thumbnail_id(); $ok=true;
+    };
+
+  ?>
+
+<?php if ($ok) : ?>
+  <?php
+    $imci_s = wp_get_attachment_image_src( $ima, 'banner_small');
+    $imci_m = wp_get_attachment_image_src( $ima, 'banner_medium');
+    $imci = wp_get_attachment_image_src( $ima, 'banner');
+  ?>
+  <style>
     .banner {
-      background-image:url('<?= $imci_m['0']; ?>');
+      background-image: url('<?= $imci_s['0']; ?>');
     }
-  }
-  @media only screen and (min-width: 1280px) {
-    .banner {
-      background-image:url('<?= $imci['0']; ?>');
+    @media only screen and (min-width: 768px) {
+      .banner {
+        background-image:url('<?= $imci_m['0']; ?>');
+      }
     }
-  }
-</style>
+    @media only screen and (min-width: 1280px) {
+      .banner {
+        background-image:url('<?= $imci['0']; ?>');
+      }
+    }
+  </style>
 <?php endif; ?>
