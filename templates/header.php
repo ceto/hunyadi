864f9504d6a24e327d539694e8c2bbf938ace536
Szabó Gábor <szabogabor@hydrogene.hu>
2017-06-27 14:56:08 +0200

    ajanlatkero a sidebarba

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,20 @@
 <header class="theleftside">
+
   <?php do_action('wpml_add_language_selector'); ?>
+
   <div class="titlearea">
     <a href="<?= esc_url(home_url('/')); ?>" class="sitename" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a>
   </div>
 
 
-
   <nav class="nav nav--side" role="navigation">
+
       <?php
       if (has_nav_menu('primary_navigation')) :
         wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'menu menu--pri']);
       endif;
       ?>
-
+    <a class="right-off-canvas-toggle sideroctl" href="#">Ajánlat kérése</a>
       <?php
       if (has_nav_menu('secondary_navigation')) :
         wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'menu menu--sec']);
