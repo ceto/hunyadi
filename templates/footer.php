743bf5c10f8f0c048bcd06cc8176789aa6cad3cd
Szabó Gábor <szabogabi@gmail.com>
2018-06-12 15:01:31 +0200

    privacy link

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="content-info pagesection pagesection--narrow" role="contentinfo">
   <div class="row">
-    <div class="columns medium-10 medium-centered"><span class="footer__copy">&copy; <?= date(Y) ?> HUNYADI Kft. &middot; <?php _e('Minden jog fenntartva.', 'hunyadi') ?></span> <span class="footer__madeby">by <a href="http://hydrogene.hu" target="_blank">Hydrogene</a></span></div>
+    <div class="columns medium-10 medium-centered"><span class="footer__copy">&copy; <?= date(Y) ?> HUNYADI Kft. &middot; <?php _e('Minden jog fenntartva.', 'hunyadi') ?> &middot; <a href="<?= get_the_permalink(464) ?>"><?php _e('Adatvédelem', 'hunyadi') ?></a> </span> <span class="footer__madeby">by <a href="http://hydrogene.hu" target="_blank">Hydrogene</a></span></div>
     <?php //dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
