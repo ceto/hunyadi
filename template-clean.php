cb7ba8ee8294256c9febb79394eff9360d926076
Szabó Gábor <szabogabi@gmail.com>
2019-06-14 13:20:41 +0200

    ajanlatkero

---
+++ template-clean.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: Fazisjavitas Ajánlatkérő Template
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+<main class="main" role="main">
+
+    <section class="pagesection pagesection--fullheight">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfiHLGQPA8hxl2oZpHF3zygQTIJuWOE_2QlnrehvwkzUEEBJA/viewform?embedded=true"
+            frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    </section>
+
+</main><!-- /.main -->
+<?php endwhile; ?>
